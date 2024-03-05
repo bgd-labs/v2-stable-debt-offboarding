6cac13b44d956bc4bc22de3dd6c86adb1d9f79b2
sakulstra <lukasstrassel@googlemail.com>
2024-03-05 11:54:34 +0100

    fix: update avalanche

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 make download chain=1 address=0xb9184a4480830bf89B55b73631E287Df9079F466 # amm
 make download chain=1 address=0x085E34722e04567Df9E6d2c32e82fd74f3342e79 # proto
 make download chain=137 address=0x1685d81212580dd4cda287616c2f6f4794927e18
-make download chain=43114 address=0x4F01AeD16D97E3aB5ab2B501154DC9bb0F1A5A2C
+make download chain=43114 address=0x102Bf2C03c1901AdBA191457A8c4A4eF18b40029
