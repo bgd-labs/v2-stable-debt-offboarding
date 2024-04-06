73ff80c5da8da082201cb4cc94d86798d3c95f00
sakulstra <lukasstrassel@googlemail.com>
2024-04-06 22:34:46 +0200

    fix: add libraries

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.6.0;
 
 import {Script} from 'forge-std/Script.sol';
-import {LendingPool} from 'src/contracts/protocol/lendingpool/LendingPool.sol';
+import {AaveV2Ethereum} from 'aave-address-book/AaveV2Ethereum.sol';
+import {LendingPool, ILendingPoolAddressesProvider} from 'src/contracts/protocol/lendingpool/LendingPool.sol';
 
 //  command: make deploy-ledger contract=scripts/Deploy.s.sol:Deploy chain=mainnet
 contract Deploy is Script {
   function run() external {
     vm.startBroadcast();
-    new LendingPool();
+    LendingPool pool = new LendingPool();
+    pool.initialize(ILendingPoolAddressesProvider(address(AaveV2Ethereum.POOL_ADDRESSES_PROVIDER)));
     vm.stopBroadcast();
   }
 }
