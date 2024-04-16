9d98a16a5c019b266488198286e9d8a491eb3a5a
sakulstra <lukasstrassel@googlemail.com>
2024-04-16 15:26:28 +0200

    fix: update download

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 make download chain=1 address=0xb9184a4480830bf89B55b73631E287Df9079F466 # amm
 make download chain=1 address=0x085E34722e04567Df9E6d2c32e82fd74f3342e79 # proto
+make download chain=1 address=0x02D84abD89Ee9DB409572f19B6e1596c301F3c81 # new proto
 make download chain=137 address=0x1685d81212580dd4cda287616c2f6f4794927e18
 make download chain=43114 address=0x102Bf2C03c1901AdBA191457A8c4A4eF18b40029
